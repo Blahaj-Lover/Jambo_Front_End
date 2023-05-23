510f3301ac73aa89f07d47f442c836db78f46108
Owen Farida <110817879+Vilosofy@users.noreply.github.com>
2023-05-23 21:06:17 +0700

    setup tailwind

---
+++ jambo-fe/src/components/explorePage/Popular.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const Popular = () => {
+    return (
+        <div className="Popular">
+            
+        </div>
+    );
+};
+
+export default Popular;

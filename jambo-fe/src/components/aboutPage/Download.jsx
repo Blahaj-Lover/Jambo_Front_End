3ad6358de95b00e6d6ee96f48616745cd2feb996
Owen Farida <110817879+Vilosofy@users.noreply.github.com>
2023-06-07 15:18:01 +0700

    benerin font, page about, page payment, hover effect, link button

--- jambo-fe/src/components/aboutPage/Download.jsx
+++ jambo-fe/src/components/aboutPage/Download.jsx
@@ -8,32 +8,32 @@ const Download = () => {
         <div className="Download">
             <div className="box bg-[#FFD7C3] w-[1125px] h-[179px] outline rounded">
 
-                <div className="flex flex-col gap-[13px] pt-[27px] pb-[27px]">
-                    <h1 className="text-[30px] font-bold text-center"> Download Spirit Well Now </h1>
+                <div className="britanica flex flex-col gap-[13px] pt-[27px] pb-[27px]">
+                    <h1 className="text-[35px] font-bold text-center"> Download Spirit Well Now </h1>
 
                     <div className="flex gap-[13px] justify-center">
 
-                        <div className="boxes flex gap-[8px] bg-[#FDD400] w-[231px] h-[56px] outline rounded items-center justify-center">
+                        <div className="boxes flex gap-[8px] bg-[#FDD400] w-[231px] h-[56px] outline rounded items-center justify-center cursor-pointer">
                             <button className="flex gap-[8px]">
                                 <Windows />
-                                <text className="text-[14px] font-bold"> Download </text>
-                                <text className="text-[14px]"> (2GB) </text>
+                                <text className="text-[14px] font-bold font-jakarta"> Download </text>
+                                <text className="text-[14px] font-jakarta"> (2GB) </text>
                             </button>
                         </div>
 
-                        <div className="boxes flex gap-[8px] bg-[#FDD400] w-[231px] h-[56px] outline rounded items-center justify-center">
+                        <div className="boxes flex gap-[8px] bg-[#FDD400] w-[231px] h-[56px] outline rounded items-center justify-center cursor-pointer">
                             <button className="flex gap-[8px]">
                                 <Apple />
-                                <text className="text-[14px] font-bold"> Download </text>
-                                <text className="text-[14px]"> (2GB) </text>
+                                <text className="text-[14px] font-bold font-jakarta"> Download </text>
+                                <text className="text-[14px] font-jakarta"> (2GB) </text>
                             </button>
                         </div>
 
                         <div className="boxes flex gap-[8px]">
-                            <button className="flex gap-[8px] bg-[#FDD400] w-[231px] h-[56px] outline rounded items-center justify-center">
+                            <button className="flex gap-[8px] bg-[#FDD400] w-[231px] h-[56px] outline rounded items-center justify-center cursor-pointer">
                                 <Linux />
-                                <text className="text-[14px] font-bold"> Download </text>
-                                <text className="text-[14px]"> (2GB) </text>
+                                <text className="text-[14px] font-bold font-jakarta"> Download </text>
+                                <text className="text-[14px] font-jakarta"> (2GB) </text>
                             </button>
                         </div>
 
